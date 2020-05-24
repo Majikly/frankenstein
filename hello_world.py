6694e585426d9bed552865f77872dead3c42546b
Michael <65255696+Majikly@users.noreply.github.com>
2020-05-24 14:27:19 -0400

    Add files via upload
    Initial Setup of frankenstein repository
    05/24/2020

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+'''
+Print "Hello World!
+
+Michael Koptie
+May 24, 2020
+Intermediate Python Githup Repository setup.
+
+
+'''
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
